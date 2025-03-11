82c90a13852e9baf48bd59dcee7470de9b67bfb8
Elvira Wiklund <wiklundel@gmail.com>
2025-03-11 16:51:01 +0100

    Shuffle hästbilder

--- randomizer.js
+++ randomizer.js
@@ -1,3 +1,16 @@
 function myFunction() {
     alert("Tack för meddelandet!");
   }
+
+// Function to shuffle the array (Fisher-Yates Shuffle)
+function shuffleImg() {
+    const img = document.getElementsByClassName("hästbilder");
+    const items = Array.from(img.children);
+
+    for (let i = items.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+    }
+    items.forEach(item => img.appendChild(items));
+
+}
