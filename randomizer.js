8df9831dbf91eb891fed11f99af2ae4b5bc69387
Elvira Wiklund <wiklundel@gmail.com>
2025-03-11 17:19:47 +0100

    Uppdaterat ShuffleImg igen igen

--- randomizer.js
+++ randomizer.js
@@ -4,13 +4,18 @@ function myFunction() {
 
 // Function to shuffle the array (Fisher-Yates Shuffle)
 function shuffleImg() {
+    
     const img = document.getElementsByClassName("hästbilder");
-    const items = Array.from(img.children);
 
-    for (let i = items.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+    for (let container of imgContainers) {
+        const items = Array.from(container.children); // Convert HTMLCollection to an array
+
+        for (let i = items.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+        }
+        items.forEach(item => container.appendChild(item));
     }
-    items.forEach(items => img.appendChild(items));
-    window.onload = shuffleGrid;
+
+    window.onload = shuffleImg;
 }
