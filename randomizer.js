f09bc6ed8d3b9c1332b7fa07c7786592e1505aa4
Elvira Wiklund <wiklundel@gmail.com>
2025-03-13 17:16:34 +0100

    Uppdaterat allt

--- randomizer.js
+++ randomizer.js
@@ -1,28 +1,46 @@
+// Kontakt
+// Bekräftelse när meddelande skickats
 const form = document.querySelector('form');
-form.addEventListener('submit', myFunction);
 
-function myFunction() {
-    alert("Tack för meddelandet!");
+if (form) {
+    form.addEventListener('submit', myFunction);
+    function myFunction() {
+        alert("Tack för meddelandet!");
   }
+}
 
-// Function to shuffle the array (Fisher-Yates Shuffle)
-function shuffleImg() {
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Våra hästar
+// Shuffla hästbilder
+  function shuffleImg() {
     
     const containers = document.getElementsByClassName("hästbilder");
+    if(containers) {
+        for (let container of containers) {
+            const items = Array.from(container.children); // Convert HTMLCollection to an array
 
-    for (let container of containers) {
-        const items = Array.from(container.children); // Convert HTMLCollection to an array
+            const newItems = [];
 
-        const newItems = [];
+            while (items.length > 0 ) {
+                let j = Math.floor(Math.random() * items.length);
+                let v = items[j];
+                items.splice(j, 1);
 
-        while (items.length > 0 ) {
-            let j = Math.floor(Math.random() * items.length);
-            let v = items[j];
-            items.splice(j, 1);
-
-            newItems.push(v);
+                newItems.push(v);
+            }
+            newItems.forEach(item => container.appendChild(item));
         }
-        newItems.forEach(item => container.appendChild(item));
     }
 }
 window.onload = shuffleImg;
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+// sidebar
+const btn = document.querySelector('#btn');
+const sidebar = document.querySelector('.sidebar');
+
+if(btn && sidebar) {
+    btn.addEventListener('click', function () {
+        sidebar.classList.toggle('active');
+    });
+}
