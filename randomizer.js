2c75713ecf4657c67610bbe650f89170ba1de7df
Elvira Wiklund <wiklundel@gmail.com>
2025-03-14 13:59:22 +0100

    Allt klart med js:)

--- randomizer.js
+++ randomizer.js
@@ -36,11 +36,45 @@ window.onload = shuffleImg;
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 // sidebar
-const btn = document.querySelector('#btn');
-const sidebar = document.querySelector('.sidebar');
+const btn = document.querySelector('.btn');
+const sidemenu = document.querySelector('.sidemenu');
+const sidebar = document.querySelector('#sidebar');
 
-if(btn && sidebar) {
-    btn.addEventListener('click', function () {
-        sidebar.classList.toggle('active');
-    });
+if(btn && sidemenu) {
+    function updateIcon() {
+        if (sidemenu.classList.contains("active")) {
+            btn.setAttribute("icon", "ph:caret-double-left-light");
+        } else {
+            btn.setAttribute("icon", "ph:list-light"); 
+        }
+    }
+    updateIcon();
+    
+    if(window.matchMedia('(max-width: 900px)').matches && btn && sidemenu) {
+        btn.addEventListener('click', function () {
+            sidemenu.classList.toggle('active');
+            updateIcon();
+        });
+    } else {
+        btn.addEventListener('click', function () {
+            sidebar.classList.toggle('active');
+            updateIcon();
+        });
+    }
 }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+// header
+
+// Add box shadow to header on scroll
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+
